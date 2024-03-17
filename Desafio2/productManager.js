3dc205bd2d42be9d06d48f9c618f522385e23889
jrojo66 <javier.rojo66@gmail.com>
2024-03-17 12:27:37 -0300

    Desafio 2 v3

--- Desafio2/productManager.js
+++ Desafio2/productManager.js
@@ -1,14 +1,45 @@
-// Codigo
+const fs = require("fs");
+const { stringify } = require("querystring");
+// Codigo 
 class ProductManager {
     #products;
+    #filePath;
   constructor() {
-    this.#products = [];
+    this.#filePath = "./files/products.json"
+    this.#products = this.#getProductsFromFile();
   }
+
+  // CRUD - Create, Read, Update, Delete
+  // Create
+  #getProductsFromFile = () => {
+    try{
+      if(fs.existsSync(this.#filePath)){
+        return JSON.parse(fs.readFileSync(this.#filePath), {encoding: "utf-8"})
+      } else {
+      return [];
+      }
+    } catch(error) {
+      console.log("Error reading file:  ",error);
+    }
+  }
+
+  #saveProductsInFile(){
+    try {
+      fs.writeFileSync(this.#filePath, JSON.stringify(this.#products))
+    } catch (error) {
+      console.log("Error saving file:  ",error);
+    }
+  }
+
   addProduct = (title, description, price, thumbnail, code, stock) => {
       const existingProduct = this.#products.find(element => element.code === code);
     if (!existingProduct) {
-      let id = this.#products.length + 1;
+      let id = 1;
+      if(this.#products.length !== 0){
+        id = this.#products[this.#products.length-1].id+1; // creates id = lastProduct.id +1
+      }
       this.#products.push({id, title, description, price, thumbnail, code, stock});
+      this.#saveProductsInFile();
       console.log("Product successfully added")
     } else {
       console.log(
@@ -16,9 +47,11 @@ class ProductManager {
       );
     }
   };
+  //  Read all
   getProducts = () => {
     return this.#products;
   };
+  // Read by Id
   getProductById = (id) => {
     const product = this.#products.find(element => element.id === id);
     if (product) {
@@ -27,6 +60,34 @@ class ProductManager {
       console.log("Not found");
     }
   };
+// Update
+updateProduct = (id, objectUpdate) => {
+  const index = this.#products.findIndex(x => x.id === id);
+  if(index >= 0){
+    const {id, ...rest} = objectUpdate;
+    this.#products[index] = {...this.#products[index],...rest};
+    this.#saveProductsInFile();
+    console.log("Producto "+id+" actualizado!");
+  }
 }
+
+// Delete
+deleteProductById = (id) => {
+  const index = this.#products.findIndex(x => x.id === id);
+  if(index >= 0){
+    this.#products = this.#products.filter(x=> x.id !== id) ;
+    this.#saveProductsInFile();
+    console.log("Producto: "+id+" eliminado");
+  } else {
+    console.log("id: "+id+" not found ")
+  }  
+}
+
+  getfilePath() {
+    return this.#filePath;
+  }
+}
+
+
 
 module.exports = ProductManager;
